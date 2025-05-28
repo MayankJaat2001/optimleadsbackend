6c2dd02545c08aa7dedb6168bdb5c2e4de15e01a
Mayank-Chaudhary <mchaudhary0000@gmail.com>
2025-05-28 18:09:57 +0530

    fixes

--- functions/server.js
+++ functions/server.js
@@ -12,8 +12,13 @@ dotenv.config({path: "./.env"});
 const app = express();
 
 const port = process.env.PORT || 5000;
-app.use(cors());
-app.options('*', cors());
+app.use(cors({
+  origin: "*", // Or use your specific domain if needed
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+// app.options('*', cors());
 // app.use(cors({
 //   origin: ["/"],
 //   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
