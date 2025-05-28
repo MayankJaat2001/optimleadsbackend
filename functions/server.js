2110f16a9e9fb270937fb08781a0fbaa14cf1645
Mayank-Chaudhary <mchaudhary0000@gmail.com>
2025-05-28 17:49:23 +0530

    fixes

--- functions/server.js
+++ functions/server.js
@@ -12,11 +12,13 @@ dotenv.config({path: "./.env"});
 const app = express();
 
 const port = process.env.PORT || 5000;
-app.use(cors({
-  origin: ["https://www.optimadata.ai"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
+app.use(cors());
+app.options('*', cors());
+// app.use(cors({
+//   origin: ["/"],
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   credentials: true
+// }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
